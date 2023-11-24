2afc31e509c7bb09409327a851e0d681022b6e96
Abdiox <abdirahmanroble2610@gmail.com>
2023-11-24 17:28:21 +0100

    Lavet Put Products (Samt at kunne ændre på color og kategori)

--- routes/products.js
+++ routes/products.js
@@ -24,11 +24,15 @@ productRouter.get("/", async (request, response) => {
 });
 
 // GET SPECIFIC PRODUCT BY ID
+// GET SPECIFIC PRODUCT BY ID
 productRouter.get("/:id", async (request, response) => {
   try {
     const productId = request.params.id;
     const query = /*sql*/ `
-      SELECT P.*, GROUP_CONCAT(DISTINCT CAT.categoryName) as categories, GROUP_CONCAT(DISTINCT CO.colorName) as colors
+      SELECT
+        P.*,
+        GROUP_CONCAT(DISTINCT CAT.categoryName) as categories,
+        GROUP_CONCAT(DISTINCT CO.colorName) as colors
       FROM Product AS P
       LEFT JOIN ProductCategory AS PC ON P.productId = PC.productId
       LEFT JOIN Category AS CAT ON PC.categoryId = CAT.categoryId
@@ -74,15 +78,15 @@ productRouter.get("/:id/category", async (request, response) => {
   }
 });
 
-// Put Products
+// Put Products (Samt at kunne ændre på color og kategori)
 productRouter.put("/:id", async (request, response) => {
   try {
     const productId = request.params.id;
 
-    // Udtræk opdaterede produktoplysninger fra anmodningens krop
-    const { productNumber, productName, imageURLs, listPrice, offerPrice, stockQuantity } = request.body;
+    // Extract updated product information from the request body
+    const { productNumber, productName, imageURLs, listPrice, offerPrice, stockQuantity, categories, colors } = request.body;
 
-    // Opdater produktet i Product-tabellen
+    // Update the product in the Product table
     const updateProductQuery = /*sql*/ `
       UPDATE Product
       SET productNumber = ?, productName = ?, imageURLs = ?, listPrice = ?, offerPrice = ?, stockQuantity = ?
@@ -93,78 +97,45 @@ productRouter.put("/:id", async (request, response) => {
 
     await dbConnection.execute(updateProductQuery, updateProductValues);
 
+    // Update categories for the product
+    if (categories && categories.length > 0) {
+      // Delete existing categories for the product
+      const deleteCategoriesQuery = /*sql*/ `
+        DELETE FROM ProductCategory WHERE productId = ?;
+      `;
+      await dbConnection.execute(deleteCategoriesQuery, [productId]);
+
+      // Insert new categories for the product
+      const insertCategoriesQuery = /*sql*/ `
+        INSERT INTO ProductCategory (productId, categoryId) VALUES (?, ?);
+      `;
+      for (const category of categories) {
+        await dbConnection.execute(insertCategoriesQuery, [productId, category]);
+      }
+    }
+
+    // Update colors for the product
+    if (colors && colors.length > 0) {
+      // Delete existing colors for the product
+      const deleteColorsQuery = /*sql*/ `
+        DELETE FROM Color WHERE productId = ?;
+      `;
+      await dbConnection.execute(deleteColorsQuery, [productId]);
+
+      // Insert new colors for the product
+      const insertColorsQuery = /*sql*/ `
+        INSERT INTO Color (productId, colorName) VALUES (?, ?);
+      `;
+      for (const color of colors) {
+        await dbConnection.execute(insertColorsQuery, [productId, color]);
+      }
+    }
+
     response.json({ message: "Product updated successfully" });
   } catch (error) {
     console.error(error);
     response.status(500).json({ message: "Internal server error" });
   }
 });
-
-// productRouter.delete("/:id", async (request, response) => {
-//   try {
-//     const productId = request.params.id;
-
-//     const deleteProductQuery = /*sql*/ `
-//       DELETE FROM Product
-//       WHERE productId = ?;
-//     `;
-
-//     await dbConnection.execute(deleteProductQuery, [productId]);
-
-//     response.json({ message: "Product deleted successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     response.status(500).json({ message: "Internal server error" });
-//   }
-// });
-
-//////////////// DET HER NEDENUNDER ER HVIS DU SKAL KUNNE OPDATERER ELLER SLETTE PROPERTIES GENNEM FLERE TABELLER: ////////////////
-
-// productRouter.put("/:id", async (request, response) => {
-//   try {
-//     const productId = request.params.id;
-
-//     // Extract updated product information from the request body
-//     const { productNumber, productName, imageURLs, listPrice, offerPrice, stockQuantity, categories, colors } = request.body;
-
-//     // Update the product in the Product table
-//     const updateProductQuery = /*sql*/ `
-//       UPDATE Product
-//       SET productNumber = ?, productName = ?, imageURLs = ?, listPrice = ?, offerPrice = ?, stockQuantity = ?
-//       WHERE productId = ?;
-//     `;
-
-//     const updateProductValues = [productNumber, productName, imageURLs, listPrice, offerPrice, stockQuantity, productId];
-
-//     await dbConnection.execute(updateProductQuery, updateProductValues);
-
-//     // Insert new categories for the product
-//     const insertCategoriesQuery = /*sql*/ `
-//       INSERT INTO ProductCategory (productId, categoryId) VALUES (?, ?);
-//     `;
-
-//     for (const category of categories) {
-//       // Ensure that the category is an integer before attempting to insert
-//       const categoryId = parseInt(category);
-//       if (!isNaN(categoryId)) {
-//         await dbConnection.execute(insertCategoriesQuery, [productId, categoryId]);
-//       }
-//     }
-
-//     // Insert new colors for the product
-//     const insertColorsQuery = /*sql*/ `
-//       INSERT INTO Color (productId, colorName) VALUES (?, ?);
-//     `;
-
-//     for (const color of colors) {
-//       await dbConnection.execute(insertColorsQuery, [productId, color]);
-//     }
-
-//     response.json({ message: "Product updated successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     response.status(500).json({ message: "Internal server error" });
-//   }
-// });
 
 export default productRouter;
