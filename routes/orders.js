c5e1b7ca08f02e6605ac3973788b54ea67e7de18
Abdiox <abdirahmanroble2610@gmail.com>
2023-11-28 13:03:56 +0100

    lavet post på orders og get

--- routes/orders.js
+++ routes/orders.js
@@ -16,4 +16,39 @@ orderRouter.get("/", async (request, response) => {
   }
 });
 
+orderRouter.get("/:id", async (request, response) => {
+  try {
+    const orderId = request.params.id;
+    const query = /*sql*/ `
+            SELECT * FROM orders WHERE orderId = ?;`;
+    const [rows, fields] = await dbConnection.execute(query, [orderId]);
+    if (rows.length === 0) {
+      response.status(404).json({ message: "Order not found" });
+    } else {
+      response.json(rows[0]);
+    }
+  } catch (error) {
+    console.log(error);
+    response.json({ message: error.message });
+  }
+});
+
+orderRouter.post("/", async (request, response) => {
+  const { userId, orderDate, totalPrice } = request.body;
+
+  const createOrderQuery = /*sql*/ `
+            INSERT INTO orders (userId, orderDate, totalPrice)
+            VALUES (?, ?, ?);
+        `;
+  const createOrderValues = [userId, orderDate, totalPrice];
+
+  try {
+    await dbConnection.execute(createOrderQuery, createOrderValues);
+    response.json({ message: "Order created successfully" });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default orderRouter;
