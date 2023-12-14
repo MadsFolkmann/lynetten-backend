d1c696d126c463d76392df02f4c1b02a3a096fd2
MadsFolkmann <mads1238@gmail.com>
2023-12-14 22:31:12 +0100

    Login

--- routes/user.js
+++ routes/user.js
@@ -37,18 +37,33 @@ userRouter.get("/:id", async (request, response) => {
 });
 
 // Post (Create) User
+// Create User
 userRouter.post("/", async (request, response) => {
   const { email, password, newsletterSubscription } = request.body;
+  console.log(email, password, newsletterSubscription);
 
-  const createUserQuery = /*sql*/ `
-    INSERT INTO users (email, password, newsletterSubscription)
-    VALUES (?, ?, ?);
+  // Check if the email already exists
+  const checkEmailQuery = /*sql*/ `
+    SELECT * FROM users WHERE email=?;
   `;
-  const createUserValues = [email, password, newsletterSubscription];
+  const checkEmailValues = [email];
 
   try {
-    await dbConnection.execute(createUserQuery, createUserValues);
-    response.json({ message: "User created successfully" });
+    const [results] = await dbConnection.execute(checkEmailQuery, checkEmailValues);
+    if (results.length > 0) {
+      // Email already exists, send a response indicating so
+      response.status(400).json({ message: "Email already in use" });
+    } else {
+      // Email is not in use, proceed with user creation
+      const createUserQuery = /*sql*/ `
+        INSERT INTO users (email, password, newsletterSubscription)
+        VALUES (?, ?, ?);
+      `;
+      const createUserValues = [email, password, newsletterSubscription];
+
+      await dbConnection.execute(createUserQuery, createUserValues);
+      response.json({ message: "User created successfully" });
+    }
   } catch (error) {
     console.error(error);
     response.status(500).json({ message: "Internal server error" });
@@ -94,5 +109,34 @@ userRouter.delete("/:id", async (request, response) => {
     response.status(500).json({ message: "Internal server error" });
   }
 });
+
+// Login User
+userRouter.post("/login", async (request, response) => {
+  const { email, password } = request.body;
+
+  //Check if the user exists
+  const findUserQuery = /*sql*/ `
+    SELECT userId, email, newsletterSubscription
+    FROM Users 
+    WHERE email=? AND password=?;
+  `;
+  const values = [email, password];
+
+  try {
+    const [results] = await dbConnection.execute(findUserQuery, values);
+    if (results.length === 0) {
+      response.status(401).json({ message: "Invalid email or password" });
+    } else {
+      const userData = results[0];
+      response.json({ message: "Login successful", user: userData });
+      console.log(userData);
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
 
 export default userRouter;
